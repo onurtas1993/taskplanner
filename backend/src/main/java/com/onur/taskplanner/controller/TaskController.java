79dd50687569a3fc5677a41ab33f384d6db2dba9
Onur <onurtas1993@gmail.com>
2026-07-18 21:03:46 +0200

    Implemented task listing

--- backend/src/main/java/com/onur/taskplanner/controller/TaskController.java
+++ backend/src/main/java/com/onur/taskplanner/controller/TaskController.java
@@ -9,10 +9,9 @@ import com.onur.taskplanner.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/tasks")
@@ -34,5 +33,12 @@ public class TaskController {
         Task task = taskService.createTask(taskRequest);
         TaskDto taskDto = taskMapper.toDto(task);
         return new ResponseEntity<>(taskDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDto>> listTasks() {
+        List<Task> tasks = taskService.listTasks();
+        List<TaskDto> taskDtos = tasks.stream().map(taskMapper::toDto).toList();
+        return new ResponseEntity<>(taskDtos, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/onur/taskplanner/service/TaskService.java
+++ backend/src/main/java/com/onur/taskplanner/service/TaskService.java
@@ -3,7 +3,11 @@ package com.onur.taskplanner.service;
 import com.onur.taskplanner.domain.CreateTaskRequest;
 import com.onur.taskplanner.domain.entity.Task;
 
+import java.util.List;
+
 public interface TaskService {
 
     Task createTask(CreateTaskRequest request);
+
+    List<Task> listTasks();
 }
