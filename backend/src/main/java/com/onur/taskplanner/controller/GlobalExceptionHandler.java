3e15ed0ad7a080864851eaba61b0666a0be06051
Onur <onurtas1993@gmail.com>
2026-07-18 23:10:11 +0200

    Handled TaskNotFoundException in GlobalExceptionHandler

--- backend/src/main/java/com/onur/taskplanner/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/onur/taskplanner/controller/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.onur.taskplanner.controller;
 
 import com.onur.taskplanner.domain.dto.ErrorDto;
+import com.onur.taskplanner.exception.TaskNotFoundException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
 
         String errorMessage = ex.getBindingResult().getFieldErrors().stream().findFirst()
@@ -17,5 +20,12 @@ public class GlobalExceptionHandler {
                 .orElse("Validation failed.");
         ErrorDto errorDto = new ErrorDto(errorMessage);
         return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorDto> handletaskNotFoundException(TaskNotFoundException ex) {
+        String msg = ex.getMessage();
+        ErrorDto errorDto = new ErrorDto(msg);
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 }
